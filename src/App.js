ed6ddd572d5356030ecac49f764f4cc7585499d1
Katy Thompson <katimation@gmail.com>
2024-03-12 13:28:42 -0500

    implemented footer section

--- src/App.js
+++ src/App.js
@@ -8,31 +8,28 @@ import {
 } from "react-router-dom";
 
 import About from './components/about';
+import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-        <div className='topnav'>
-            <div className='topnav-left'>
-                <Link to="/about">ABOUT</Link>
-            </div>
-            <div className='topnav-right'>
-                <Link to="/portfolio">PORTFOLIO</Link>
-                <Link to="/contact">CONTACT</Link>
-                <Link to="/resume">RESUME</Link>
-                <Redirect from="/" exact to="/about" />
-            </div>
-        </div>
-
+<div className="App">
+            <Router>
+                <div className='topnav'>
+                    <div className='topnav-left'>
+                        <Link to="/about">ABOUT</Link>
+                    </div>
+                </div>
         <hr />
 
         <Switch>
-            <Route exact path="/about">
-                <About />
-            </Route>
-        </Switch>
-    </Router>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                </Switch>
+            </Router>
+            <Footer/>
+        </div>
 );
 }
 
