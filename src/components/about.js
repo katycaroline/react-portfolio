79291ef8c1eef780e94d54ff9a6d3625e58e2aab
Katy Thompson <katimation@gmail.com>
2024-03-12 13:22:39 -0500

    implemented about section

---
+++ src/components/about.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Avatar from '../assets/images/avatar.png';
+import '../App.css';
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='card'>
+
+                            <div className='card-body'>
+                                <div className="row abstract-back ">
+                                    <img className='avatar' src={Avatar} />
+                                </div>
+                            </div>
+
+                            <div className='card-body'>
+                                <div className="row">
+                                    <p className='p-about'>
+                                    Hello! My name is Katy Thompson. I'm currently enrolled in a coding bootcamp and this is my portfolio. It is a work in progress.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="modal-footer" />
+
+                            <div className='card-body'>
+                                <h2 className='edu'>Education</h2>
+                                <br />
+
+                                <div className='grid-container-profile'>
+                                    <div className='grid-item'>
+                                        <div className='title-uni'>Washington University STL Coding Bootcamp</div>
+                                        <div className='title-concentration'>Full Stack Web Development</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default About;
