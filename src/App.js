7a33648cfa6a445d077650feee9e16515445d685
Katy Thompson <katimation@gmail.com>
2024-03-12 13:31:10 -0500

    implemented navbar section

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import React  from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect
-} from "react-router-dom";
-
-import About from './components/about';
 import Footer from './components/footer';
+import NavBar from './components/navBar';
 import './App.css';
 
 function App() {
   return (
 <div className="App">
-            <Router>
-                <div className='topnav'>
-                    <div className='topnav-left'>
-                        <Link to="/about">ABOUT</Link>
-                    </div>
-                </div>
-        <hr />
-
-        <Switch>
-                    <Route exact path="/about">
-                        <About />
-                    </Route>
-                </Switch>
-            </Router>
-            <Footer/>
-        </div>
+    <NavBar/>
+    <Footer/>
+</div>
 );
 }
 
